06506338f82215746f5848e5180554f22f7e963f
DigitalJay <pushkareva_lisa@mail.ru>
2020-11-14 18:11:31 +0500

    Added acts

--- app/src/main/java/com/example/jinnapp/PersonalActivity.java
+++ app/src/main/java/com/example/jinnapp/PersonalActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class PersonalActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
